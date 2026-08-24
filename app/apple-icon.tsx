354b107b6b8c27108646fe36ae0b3c0f3bb86e4a
radauradu <radauradu999@gmail.com>
2026-08-24 21:46:23 +0200

    Favicon

--- app/apple-icon.tsx
+++ app/apple-icon.tsx
@@ -1,9 +1,9 @@
 import { ImageResponse } from "next/og";
-import { PinkGlobeFavicon } from "@/lib/pinkGlobeFavicon";
+import { PinkRadioFavicon } from "@/lib/pinkRadioFavicon";
 
 export const size = { width: 180, height: 180 };
 export const contentType = "image/png";
 
 export default function AppleIcon() {
-  return new ImageResponse(<PinkGlobeFavicon />, { ...size });
+  return new ImageResponse(<PinkRadioFavicon />, { ...size });
 }
--- app/icon.tsx
+++ app/icon.tsx
@@ -1,9 +1,9 @@
 import { ImageResponse } from "next/og";
-import { PinkGlobeFavicon } from "@/lib/pinkGlobeFavicon";
+import { PinkRadioFavicon } from "@/lib/pinkRadioFavicon";
 
 export const size = { width: 512, height: 512 };
 export const contentType = "image/png";
 
 export default function Icon() {
-  return new ImageResponse(<PinkGlobeFavicon />, { ...size });
+  return new ImageResponse(<PinkRadioFavicon />, { ...size });
 }
